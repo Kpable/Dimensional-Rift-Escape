457ee5a9fe252333d2a3eaa9c1eaa4b7a4cfa607
lestherreynoso <lhreynos@gmail.com>
2018-04-21 10:00:47 -0400

    Got the player moving, some asteroids floating

--- Assets/Scripts/FloatInSpace.cs
+++ Assets/Scripts/FloatInSpace.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class FloatInSpace : MonoBehaviour {
 
+    public float speed = 3f;
+    public float roationalSpeed = 3f;
+    Rigidbody2D body;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(Random.Range(-speed, speed), Random.Range(-speed, speed));
+        body.angularVelocity = Random.Range(-roationalSpeed, roationalSpeed);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
